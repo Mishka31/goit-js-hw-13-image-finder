1ee24d706c7b07b4b07cf9324f78d9048b5543c6
Misha Krasnonos <muzishutka7@gmail.com>
2021-07-13 21:59:05 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './sass/main.scss';
+import ImageApiService from './js/apiService';
+import cardImagesTpl from './templates/card-images';
+
+const refs = {
+  galleryListEl: document.querySelector('.gallery'),
+  formEl: document.querySelector('.search-form'),
+  btnSearch: document.querySelector('.btn-search'),
+  btnMore: document.querySelector('.btn-more'),
+};
+const imgApiService = new ImageApiService();
+
+refs.formEl.addEventListener('submit', onSearch);
+refs.btnMore.addEventListener('click', onBtnMore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  imgApiService.query = e.currentTarget.elements.query.value;
+  imgApiService.resetPage();
+  imgApiService.fectchArticles(searchQuery).then(addMarkupHits);
+}
+
+function onBtnMore() {
+  imgApiService.fectchArticles(searchQuery).then(addMarkupHits);
+}
+
+function addMarkupHits(hits) {
+  refs.galleryListEl.insertAdjacentHTML('beforeend', cardImagesTpl(hits));
+}
