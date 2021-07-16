0fefff275382825db005b8e54b9e156688ffc21c
Misha Krasnonos <muzishutka7@gmail.com>
2021-07-16 17:20:02 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import './sass/main.scss';
 import ImageApiService from './js/apiService';
 import cardImagesTpl from './templates/card-images';
+// import * as basicLightbox from 'basiclightbox';
+const basicLightbox = require('basiclightbox');
 
 const refs = {
   galleryListEl: document.querySelector('.gallery'),
   formEl: document.querySelector('.search-form'),
   btnSearch: document.querySelector('.btn-search'),
   btnMore: document.querySelector('.btn-more'),
-  // element: document.querySelector('#my-element-selector'),
+  modalCloseEl: document.querySelector('.basicLightbox'),
+  body: document.querySelector('body'),
 };
 
 const imgApiService = new ImageApiService();
 
 refs.formEl.addEventListener('submit', onSearch);
 refs.btnMore.addEventListener('click', onBtnMore, false);
+refs.galleryListEl.addEventListener('click', onModal);
 
 function onSearch(e) {
   e.preventDefault();
@@ -23,17 +27,13 @@ function onSearch(e) {
   imgApiService.resetPage();
   imgApiService.fectchArticles(searchQuery).then(addMarkupHits);
 }
-// refs.btnMore.addEventListener('click', onScroll, false);
-
 function onBtnMore() {
   imgApiService.fectchArticles(searchQuery).then(addMarkupHits);
 }
-
 function addMarkupHits(hits) {
   refs.galleryListEl.insertAdjacentHTML('beforeend', cardImagesTpl(hits));
   omScroll();
 }
-
 function clearRequest() {
   refs.galleryListEl.innerHTML = '';
 }
@@ -45,4 +45,10 @@ function omScroll() {
       block: 'end',
     });
   }
+}
+
+function onModal(e) {
+  let instance = basicLightbox.create(`<img src=${e.target.getAttribute('data-src')}>`);
+  instance.show();
+  basicLightbox.visible();
 }
